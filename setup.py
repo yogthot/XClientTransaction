fae6fde610bf2169d65bed45a4fadf384d885d29
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2025-04-30 15:48:17 +0530

    Update quickstart.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 SHORT_DESCRIPTION = "Twitter X-Client-Transaction-Id generator written in python."
 
 with open("requirements.txt") as file:
